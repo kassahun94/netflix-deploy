6965608ceb216a6a7048b5ab99c4bb98165f92c1
kassahun akelilu <akelilu94@gmail.com>
2024-07-11 18:14:05 -0400

    package json

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prismadb: PrismaClient | undefined;
+	var prismadb: PrismaClient | undefined;
 }
 
-const client = global.prismadb ?? new PrismaClient();
+const client = globalThis.prismadb || new PrismaClient();
 
 if (process.env.NODE_ENV === "production") {
-  global.prismadb = client;
+	globalThis.prismadb = client;
 }
 
 export default client;
