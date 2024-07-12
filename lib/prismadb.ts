839da68adff2c1b1d9b564811b6a825d52c06222
kassahun akelilu <akelilu94@gmail.com>
2024-07-11 23:18:12 -0400

    package json

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-	var prismadb: PrismaClient | undefined;
+	var prismadb: PrismaClient;
 }
 
 const client = globalThis.prismadb || new PrismaClient();
